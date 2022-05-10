f7772ba33d769b5ce218562b31e7fa0a36832479
dema <dema@dema.com>
2022-05-10 22:34:00 +0800

    push commit

--- main.go
+++ main.go
@@ -3,12 +3,5 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello Git!")
-	fmt.Println("Hello Git2!")
-	fmt.Println("Hello Git3!")
-	fmt.Println("Hello Git4!")
-	fmt.Println("master test")
-	fmt.Println("hot_fix test")
 	fmt.Println("push test")
-	fmt.Println("pull test")
 }
